7d8e4da405668626eb93c1a1bd24d49250a8ae6a
Serenajandra <serena.jandra@gmail.com>
2022-10-14 19:13:16 +0200

    aggiunta funzione per es pali

--- pali/JS/script.js
+++ pali/JS/script.js
@@ -7,23 +7,39 @@ const userWord =prompt("Per favore, inserisci una parola");
 console.log(userWord);
 let userWordRight = ""
 let userWordLeft = ""
+let result = "";
+compareItSelf (userWordRight, userWordRight);
 
-// Scompongo la parola in un Array composto dalle sue sillabe
-for (let i = 0; i < userWord.length; i++){
-    console.log(userWord[i]);
-    userWordRight = userWord[i];
+
+
+
+function compareItSelf(userWordRight, userWordLeft){
+ 
+    // Scompongo la parola in un Array composto dalle sue sillabe
+    for (let i = 0; i < userWord.length; i++){
+        console.log(userWord[i]);
+        userWordRight = userWord[i];
+        
+    }
+
+    // Creo un altro array con la stessa parola ma composto dalle sillabe in senso inverso
+    for (let j = userWord.length - 1; j>=0; j--) {
+        console.log(userWord[j]);
+        userWordLeft = userWord[j];
+        
+    }
+
+    // Confronto i due nuovi array creati
+
+    if (userWordRight === userWordLeft){
+        result = "true";
+        alert("hai scelto una parola palindroma!");
+    } else{
+        result = "false";
+        alert("Mannaggia, speravo scegliessi una parola palindroma!");
+    }
+    
+    console.log(result);
+    return result;
 }
 
-// Creo un altro array con la stessa parola ma composto dalle sillabe in senso inverso
-for (let j = userWord.length - 1; j>=0; j--) {
-    console.log(userWord[j]);
-    userWordLeft = userWord[j];
-}
-
-// Confronto i due nuovi array creati
-
-if (userWordRight === userWordLeft){
-    alert("hai scelto una parola palindroma!")
-} else{
-    alert("Peccato, speravo scegliessi una parola palindroma.")
-};
